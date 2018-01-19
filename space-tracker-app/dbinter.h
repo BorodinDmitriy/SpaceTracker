ee183d386c34601839b0a08ec6a3f97ec9b1785b
Дмитрий <>
2018-01-20 01:04:43 +0300

    более правильная версия загрузчика в бд

---
+++ space-tracker-app/dbinter.h
@@ -0,0 +1,35 @@
+#ifndef DBINTER_H
+#define DBINTER_H
+
+#include <iostream>
+#include <QList>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <QDate>
+#include <QMessageBox>
+
+#include "exceptions.h"
+#include "sqlite3/sqlite3.h"
+#include "information.h"
+
+#define DirectoryDataBase "D:/QtVit/Qt5.1.0/Tools/QtCreator/bin/TEST/build-untitled-Desktop-Debug/database/dbfile"
+
+class DBInter
+{
+    public:
+        DBInter();
+        ~DBInter();
+        QList<Information> loadAllTLE(const Date &dt);
+        QList<Information> loadAllSSR(const Date &);
+        QList<Information> loadSSR(const unsigned long &id, const Date &);
+        QList<Information> loadTLE(const unsigned long &id, const Date &dt);
+        void addTLE(QList<Information> data, const Date &date);
+        void addSSR(QList<Information>, const Date &);
+        void addTLE(TLE tl, const Date &dt);
+        void addSSR(SSR, const Date &);
+    private:
+        sqlite3* db;
+};
+
+#endif // DBINTER_H
