29f6a76bf7bf207a1c4b5d09f56f6d2931a2af6f
Дмитрий <>
2018-01-22 04:21:57 +0300

    Issues #1, #2

--- space-tracker-app/tleparser.cpp
+++ space-tracker-app/tleparser.cpp
@@ -1,8 +1,44 @@
 #include "tleparser.h"
+#include "config.h"
 
 TleParser::TleParser()
 {
 
+}
+
+std::string TleParser::formParsingNumberString(const char *str, int startPosition, int finishPosition) {
+    std::string buf;
+    for (int i = startPosition; i < finishPosition; i++)
+    {
+        buf += str[i];
+    }
+    return buf;
+}
+
+std::string TleParser::formParsingTextString(const char *str, int startPosition, int finishPosition) {
+    std::string buf;
+    for (int i = startPosition; i < finishPosition; i++)
+    {
+        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
+        {
+            buf += 'E';
+        }
+        buf += str[i];
+    }
+    return buf;
+}
+
+std::string TleParser::processFloatString(std::string &str) {
+    std::string buf = str;
+    if (((buf[0] == '+') || (buf[0] == '-')))
+    {
+        buf.insert(1, ".");
+    }
+    else
+    {
+        buf.insert(0, ".");
+    }
+    return buf;
 }
 
 // Номер в комментариях ниже - номер элемента в строке TLE
@@ -16,165 +52,73 @@ void TleParser::setName(const char *str, TLE &tl)
         buf += str[i];
         i++;
     }
-    i = 0;
-    while (buf[i] != '\0')
-    {
-        tl.name[i] = buf[i];
-        i++;
-    }
+    strncpy(tl.name, buf.c_str(), buf.size() - 1);
     tl.name[i] = '\0';
     return;
 }//1
 
 void TleParser::setID(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 2; i < 7; i++)
-    {
-        buf += str[i];
-    }
-    tl.nor_id = atoi(buf.c_str());
+    tl.nor_id = atoi(formParsingNumberString(str,TLE_ID_START_POSITION,TLE_ID_FINISH_POSITION).c_str());
     return;
 }  //2
 
 void TleParser::setClass(const char *str, TLE &tl)
 {
-    tl.cl = str[7];
+    tl.cl = str[TLE_CLASSIFICATION_START_AND_FINISH_POSITION];
     return;
 } //3
 
 void TleParser::setINS(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 9; i < 11; i++)
-    {
-        buf += str[i];
-    }
-    int i = 0;
-    while (buf[i])
-    {
-        tl.i_n_s[i] = buf[i];
-        i++;
-    }
+    std::string buf = formParsingNumberString(str,TLE_LAUNCH_YEAR_START_POSITION,TLE_LAUNCH_YEAR_FINISH_POSITION);
+    strncpy(tl.i_n_s, buf.c_str(), buf.size() - 1);
     return;
 }  //4
 
 void TleParser::setINN(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 11; i < 14; i++)
-    {
-        buf += str[i];
-    }
-    tl.i_n_n = atoi(buf.c_str());
+    tl.i_n_n = atoi(formParsingNumberString(str,TLE_LAUNCH_DATA_START_POSITION, TLE_LAUNCH_DATA_FINISH_POSITION).c_str());
     return;
 }  //5
 
 void TleParser::setINP(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 14; i < 17; i++)
-    {
-        buf += str[i];
-    }
-    int i = 0;
-    while (buf[i])
-    {
-        tl.i_n_p[i] = buf[i];
-        i++;
-    }
+    std::string buf = formParsingNumberString(str,TLE_LAUNCH_TYPE_START_POSITION,TLE_LAUNCH_TYPE_FINISH_POSITION);
+    strncpy(tl.i_n_p, buf.c_str(), buf.size() - 1);
     return;
 } //6
 
 void TleParser::setEra(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 18; i < 20; i++)
-    {
-        buf += str[i];
-    }
-    int i = 0;
-    while (buf[i])
-    {
-        tl.era[i] = buf[i];
-        i++;
-    }
+    std::string buf = formParsingNumberString(str,TLE_ERA_START_POSITION,TLE_ERA_FINISH_POSITION);
+    strncpy(tl.era, buf.c_str(), buf.size() - 1);
     return;
 } //7
 
 void TleParser::setEraT(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 20; i < 32; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    tl.era_t = std::strtod(buf.c_str(), 0);
+    tl.era_t = std::strtod(formParsingTextString(str,TLE_ERA_TYPE_START_POSITION,TLE_ERA_TYPE_FINISH_POSITION).c_str(), 0);
     return;
 } //8
 
 void TleParser::setDer(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 33; i < 43; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    tl.der = std::strtod(buf.c_str(), 0);
+    tl.der = std::strtod(formParsingTextString(str,TLE_FIRST_DERIVATIVE_START_POSITION,TLE_FIRST_DERIVATIVE_FINISH_POSITION).c_str(), 0);
     return;
 }  //9
 
 void TleParser::setDer2(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 44; i < 52; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    if (((buf[0] == '+') || (buf[0] == '-')))
-    {
-        buf.insert(1, ".");
-    }
-    else
-    {
-        buf.insert(0, ".");
-    }
-    tl.der2 = std::strtod(buf.c_str(), 0);
+    std::string buf = formParsingTextString(str,TLE_SECOND_DERIVATIVE_START_POSITION,TLE_SECOND_DERIVATIVE_FINISH_POSITION);
+    tl.der2 = std::strtod(processFloatString(buf).c_str(), 0);
     return;
 }  //10
 
 void TleParser::setB(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 53; i < 61; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    if (((buf[0] == '+') || (buf[0] == '-')))
-    {
-        buf.insert(1, ".");
-    }
-    else
-    {
-        buf.insert(0, ".");
-    }
-    tl.b = std::strtod(buf.c_str(), 0);
+    std::string buf = formParsingTextString(str,TLE_FLOW_TYPE_START_POSITION,TLE_FLOW_TYPE_FINISH_POSITION);
+    tl.b = std::strtod(processFloatString(buf).c_str(), 0);
     return;
 }  //11
 
@@ -186,133 +130,62 @@ void TleParser::setEfi(const char *str, TLE &tl)
 
 void TleParser::setNum(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 64; i < 68; i++)
-    {
-        buf += str[i];
-    }
-    tl.num = atoi(buf.c_str());
+    tl.num = atoi(formParsingNumberString(str,TLE_NUM_TYPE_START_POSITION,TLE_NUM_TYPE_FINISH_POSITION).c_str());
     return;
 }  //13
 
 void TleParser::setSum(const char *str, TLE &tl)
 {
-    tl.sum = str[68] - (int)'0';
+    tl.sum = str[TLE_CHECK_SUM_START_AND_FINISH_POSITION] - (int)'0';
     return;
 }  //14
 
 void TleParser::setIncl(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 8; i < 16; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    tl.incl = std::strtod(buf.c_str(), 0);
+    tl.incl = std::strtod(formParsingTextString(str,TLE_INCLINATION_START_POSITION,TLE_INCLINATION_FINISH_POSITION).c_str(), 0);
     return;
 } //3
 
 void TleParser::setLg(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 17; i < 25; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    tl.lg = std::strtod(buf.c_str(), 0);
+    tl.lg = std::strtod(formParsingTextString(str,TLE_INCLINATION_TO_THE_RIGHT_START_POSITION,TLE_INCLINATION_TO_THE_RIGHT_FINISH_POSITION).c_str(), 0);
     return;
 }  //4
 
 void TleParser::setEx(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 26; i < 33; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    if (((buf[0] == '+') || (buf[0] == '-')))
-    {
-        buf.insert(1, ".");
-    }
-    else
-    {
-        buf.insert(0, ".");
-    }
-    tl.ex = std::strtod(buf.c_str(), 0);
+    std::string buf = formParsingTextString(str,TLE_EXCENTRICITY_START_POSITION,TLE_EXCENTRICITY_FINISH_POSITION);
+    tl.ex = std::strtod(processFloatString(buf).c_str(), 0);
     return;
 }  //5
 
 void TleParser::setArgPer(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 34; i < 42; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    tl.arg_per = std::strtod(buf.c_str(), 0);
+    tl.arg_per = std::strtod(formParsingTextString(str,TLE_PERIGEE_START_POSITION,TLE_PERIGEE_FINISH_POSITION).c_str(), 0);
     return;
 }//6
 
 void TleParser::setAverAnom(const char *str, TLE &tl)
 {
-     std::string buf;
-     for (int i = 43; i < 50; i++)
-     {
-         if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-         {
-             buf += 'E';
-         }
-         buf += str[i];
-     }
-     tl.aver_anom = std::strtod(buf.c_str(), 0);
+     tl.aver_anom = std::strtod(formParsingTextString(str,TLE_MEAN_ANOMALY_START_POSITION,TLE_MEAN_ANOMALY_FINISH_POSITION).c_str(), 0);
      return;
 }//7
 
 void TleParser::setFreq(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 52; i < 63; i++)
-    {
-        if (((str[i] == '+') || (str[i] == '-')) && (!buf.empty()))
-        {
-            buf += 'E';
-        }
-        buf += str[i];
-    }
-    tl.freq = std::strtod(buf.c_str(), 0);
+    tl.freq = std::strtod(formParsingTextString(str,TLE_MEAN_ANOMALY_START_POSITION,TLE_MEAN_ANOMALY_FINISH_POSITION).c_str(), 0);
     return;
 }//8
 
 void TleParser::setEraNum(const char *str, TLE &tl)
 {
-    std::string buf;
-    for (int i = 63; i < 68; i++)
-    {
-        buf += str[i];
-    }
-    tl.era_num = atoi(buf.c_str());
+    tl.era_num = atoi(formParsingNumberString(str,TLE_ERA_NUM_START_POSITION,TLE_ERA_NUM_START_POSITION).c_str());
     return;
 }//9
 
 void TleParser::setSum2(const char *str, TLE &tl)
 {
-    tl.sum_2 = str[68];
+    tl.sum_2 = str[TLE_CHECK_SUM_START_AND_FINISH_POSITION];
     return;
 }//10
 
@@ -416,7 +289,7 @@ QList<Information> TleParser::getTLE(const char *str)
         someInfo.Type = TLEInfo;
         if(FileOperations::IsFirstLineValid(str1) && FileOperations::IsSecondLineValid(str2))
         {
-                resInfo.append(someInfo);
+            resInfo.append(someInfo);
         }
         bufStr = getNextTLE(bufStr);
     }
